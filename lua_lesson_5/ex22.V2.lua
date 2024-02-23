a6fb9ee563e8214a79bf6367d25328614ba22217
Александр Тарасевич <66253091+AlexTarasevich@users.noreply.github.com>
2024-02-23 15:22:26 +0300

    Update ex22.V2.lua

--- lua_lesson_5/ex22.V2.lua
+++ lua_lesson_5/ex22.V2.lua
@@ -1,3 +1,6 @@
+local sub_x, sub_y, sum
+
+
 print("Enter robot's position")
 local r_x = assert(tonumber(io.read('*l')), 'Need number!')
 local r_y = assert(tonumber(io.read('*l')), 'Need number!')
@@ -8,154 +11,96 @@ local t_x = assert(tonumber(io.read('*l')), 'Need number!')
 local t_y = assert(tonumber(io.read('*l')), 'Need number!')
 
 
--- РОБОТ x - 5 y - 4
--- Нужная точка  x - 3 y - 2
+-- Необходимо  дополнеиение 
+-- Программа работает только если точка куданужно переместится и точка робота находядся в одной плоскости координат
 
 
-
--- север (north),
--- северо-восток (north-east),
--- северо-запад (north-west),
--- юг (south), юго-восток (south-east),
--- юго-запад (south-west),
--- восток (east),
--- запад (west).
-
---ГОРИЗОНТАЛЬ
---Если стартуем из точки 0.0 то по 1 прибавляем к x y пока точки робота и искомой не сойдутся 
-
--- ДОДЕЛАТЬ
 
 -- Для оси Север Восток
+-- Значение х и у должны быть положительными
 if r_x > 0 and r_y > 0 and t_x > 0 and r_x > 0 then
-    if r_x < t_x then
-        repeat
-            r_x = r_x + 1
-        until r_x == t_x
-        for i = 1, r_x do
-            print('east')
-        end
-    elseif r_x > t_x then
-        r_x = r_x - t_x
-        for i = 1, r_x do
-            print('east')
-        end
+    if r_x > t_x then
+        sub_x = r_x - t_x
+    elseif t_x > r_x then
+        sub_x = t_x - r_x
     end
 
-    if r_y < t_y then
-        repeat
-            r_y = r_y + 1
-        until r_y == t_y
-        for i = 1, r_y do
-            print('north')
-        end
-    elseif r_y > t_y then
-        r_y = r_y - t_y
-        for i = 1, r_y do
-            print('north')
-        end
+    if r_y > t_y then
+        sub_y = r_y - t_y
+    elseif t_y > r_y then
+        sub_y = t_y - r_y
+    end
+
+    sum = sub_y + sub_x
+    for i = 0, sum do
+        print("north-east")
     end
 end
 
 
 
---Для оси Восток Юг
+-- Для оси Восток Юг
+-- Значение х должны быть положительными, значения у должны быть отрицательные
 if r_x > 0 and t_x > 0 and r_y < 0 and t_y < 0 then
-    
-    if r_x < t_x then
-        repeat
-            r_x = r_x + 1
-            print('east')
-        until r_x == t_x
-        
-    elseif r_x > t_x then
-        repeat
-            r_x = r_x - 1
-            
-            print('east')
-        until r_x == t_x
-
-    
+    if r_x > t_x then
+        sub_x = r_x - t_x
+    elseif t_x > r_x then
+        sub_x = t_x - r_x
     end
 
-    if r_y < t_y then
-        repeat
-            r_y = r_y - 1
-            print('west')
-        until r_y == t_y
-       
-    elseif r_y > t_y then
-        repeat
-            print('west')
-            r_y = r_y - 1
-        until r_y == t_y
-        
+    if r_y > t_y then
+        sub_y = r_y - t_y
+    elseif t_y > r_y then
+        sub_y = t_y - r_y
+    end
+
+    sum = sub_y + sub_x
+    for i = 0, sum do
+        print("south-east")
     end
 end
 
 
 -- Для оси Запад Юг
+-- Значение х и у должны быть отрицательными
 if r_x < 0 and t_x < 0 and r_y < 0 and t_y < 0 then
-    
-    if r_x < t_x then
-        repeat
-            r_x = r_x + 1
-            print('west')
-        until r_x == t_x
-        
-    elseif r_x > t_x then
-        repeat
-            r_x = r_x - 1
-            
-            print('west')
-        until r_x == t_x
+    if r_x > t_x then
+        sub_x = r_x - t_x
+    elseif t_x > r_x then
+        sub_x = t_x - r_x
     end
 
-    if r_y < t_y then
-        repeat
-            r_y = r_y - 1
-            print('south')
-        until r_y == t_y
-       
-    elseif r_y > t_y then
-        repeat
-            print('south')
-            r_y = r_y - 1
-        until r_y == t_y
-        
+    if r_y > t_y then
+        sub_y = r_y - t_y
+    elseif t_y > r_y then
+        sub_y = t_y - r_y
+    end
+
+    sum = sub_y + sub_x
+    for i = 0, sum do
+        print("south-west")
     end
 end
 
 
 
 -- Для оси Запад Север
+-- Значение х должны быть отрицательными, значения у должны быть положительными
 if r_x < 0 and t_x < 0 and r_y > 0 and t_y > 0 then
-    
-    if r_x < t_x then
-        repeat
-            r_x = r_x + 1
-            print('west')
-        until r_x == t_x
-        
-    elseif r_x > t_x then
-        repeat
-            r_x = r_x - 1
-            
-            print('west')
-        until r_x == t_x
+    if r_x > t_x then
+        sub_x = r_x - t_x
+    elseif t_x > r_x then
+        sub_x = t_x - r_x
     end
 
-    if r_y < t_y then
-        repeat
-            r_y = r_y + 1
-            print('north')
-        until r_y == t_y
-       
-    elseif r_y > t_y then
-        repeat
-            print('north')
-            r_y = r_y - 1
-        until r_y == t_y
-        
+    if r_y > t_y then
+        sub_y = r_y - t_y
+    elseif t_y > r_y then
+        sub_y = t_y - r_y
+    end
+
+    sum = sub_y + sub_x
+    for i = 0, sum do
+        print("north-west")
     end
 end
