ab4322cde4db7ea52f5202920d326dd624f75354
Александр Тарасевич <66253091+AlexTarasevich@users.noreply.github.com>
2024-03-06 08:51:39 +0300

    Update ex17.lua

--- lua_lesson_4/ex17.lua
+++ lua_lesson_4/ex17.lua
@@ -12,11 +12,28 @@ print("Введите значение c")
 local c = assert(tonumber(io.read('*l')), 'Need number!')
 
 print("Введите начало диапозона х")
-local x1 = assert(tonumber(io.read('*l')), 'Need number!')
+local range_start = assert(tonumber(io.read('*l')), 'Need number!')
 
 print("Введите конец диапозона х")
-local x2 = assert(tonumber(io.read('*l')), 'Need number!')
+local range_end = assert(tonumber(io.read('*l')), 'Need number!')
 
-local def = a * x^2 + b * x + c
 
--- Сделать последовательный поиск и посмотреть что минимум, а что максимум
+
+local minValue = math.huge
+local maxValue = -math.huge
+
+-- math.huge - Вещественное значение, которое больше любого другого числового значения.
+
+
+for x = range_start, range_end, 0.0001 do
+    local y = a * (x ^ 2) + b * x + c
+    if y < minValue then
+        minValue = y
+    end
+    if y > maxValue then
+        maxValue = y
+    end
+end
+
+print("Значение максимум: " .. minValue)
+print("Значение минимум: " .. maxValue)
