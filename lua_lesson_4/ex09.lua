ad2b852a26327a3a250758511a5204e5acbab370
Александр Тарасевич <66253091+AlexTarasevich@users.noreply.github.com>
2024-03-06 08:45:08 +0300

    Update ex09.lua

--- lua_lesson_4/ex09.lua
+++ lua_lesson_4/ex09.lua
@@ -3,23 +3,21 @@
 
 
 for i = 1000, 9999 do
+    local fiveOrSix = false
 
+    local num = i
+    while num > 0 do
+        local digit = num % 10
+        if digit == 5 or digit == 6 then
+            fiveOrSix = true
+            break
+        end
+        num = math.floor(num / 10)
+    end
 
-    local x1 =i % 1000
-    local z1 = (i - x1) / 1000
-    
-    local x2 = x1 % 100
-    local z2 = (x1 - x2) / 100
-    
-    local x3 = x1 % 10
-    local z3 = (x2 - x3) / 10
-
-    local z4 = i % 10
-
-   if z1 ~= 5 and z2 ~= 5 and z3 ~= 5 and z4 ~= 5 and z1 ~= 6 and z2 ~= 6 and z3 ~= 6 and z4 ~= 6 then
-    print(i)
-    
-   end
+    if not fiveOrSix then
+        print(i)
+    end
 end
 
 
