8e382f0df1894b224620ebbd900354593a856469
Александр Тарасевич <66253091+AlexTarasevich@users.noreply.github.com>
2024-03-08 17:10:30 +0300

    Update ex20.lua

--- lua_lesson_4/ex20.lua
+++ lua_lesson_4/ex20.lua
@@ -2,37 +2,30 @@
 -- идущих подряд билета оказаться несчастливыми?
 
 
-local not_lucky = false
-local sum
+local ticket1 = 100000
+local ticket2 = ticket1 + 1
 
-for n = 100000, 999999 do
-   local x1 = n % 100000
-   local z1 = (n - x1) / 100000
-   
-   local x2 = x1 % 10000
-   local z2 = (x1 - x2) / 10000
-   
-   local x3 = x1 % 1000
-   local z3 = (x2 - x3) / 1000 
-
-   local x4 = x1 % 100
-   local z4 = (x3 - x4) / 100
-
-   local x5 = x1 % 10
-   local z5 = (x4 - x5) / 10
-
-   local z6 = n % 10
-
-   sum = z1 + z2 + z3 + z4 + z5 + z6
-
-   if  sum % 13 == 0 then
-      if not_lucky == true then
-         print("Да")
-      end
-      not_lucky = true
-   else
-      not_lucky = false
-   end
+while ticket1 < 999999 do
+    local sum1 = 0
+    local n = ticket1
+    while n > 0 do
+        local digit = n % 10
+        sum1 = sum1 + digit
+        n = math.floor(n / 10)
+    end
+    local sum2 = 0
+    n = ticket2
+    while n > 0 do
+        local digit = n % 10
+        sum2 = sum2 + digit
+        n = math.floor(n / 10)
+    end
+    if sum1 % 13 == 0 and sum2 % 13 == 0 then
+        print("Найдены два счастливых билета : " .. ticket1 .. " и " .. ticket2)
+        break
+    end
+    ticket1 = ticket1 + 1
+    ticket2 = ticket2 + 1
 end
 
 
