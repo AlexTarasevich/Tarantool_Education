26621f4527432da4e1dd42a8fbd930ff025f8db9
Александр Тарасевич <66253091+AlexTarasevich@users.noreply.github.com>
2024-03-22 12:04:39 +0300

    Update ex09.lua

--- lua_lesson_4/ex09.lua
+++ lua_lesson_4/ex09.lua
@@ -1,23 +1,21 @@
 -- Вывести на экран числа от 1000 до 9999 такие, что среди цифр нет цифр 5 и цифры 6.
 
 
-
-for i = 1000, 9999 do
-    local fiveOrSix = false
-
-    local num = i
-    while num > 0 do
-        local digit = num % 10
-        if digit == 5 or digit == 6 then
-            fiveOrSix = true
-            break
-        end
-        num = math.floor(num / 10)
+for i = 1000, 9999 do 
+    local has_five_or_six = false
+    local j = i
+    while j > 0 do
+      local digit = j % 10
+      if digit == 5 or digit == 6 then
+        has_five_or_six = true
+        break
+      end
+      j = j / 10
     end
-
-    if not fiveOrSix then
-        print(i)
+  
+    if not has_five_or_six then
+      print(i)
     end
-end
+  end
 
 
