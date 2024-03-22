8e9025823a26409628724f50e8b1814480905a5e
Александр Тарасевич <66253091+AlexTarasevich@users.noreply.github.com>
2024-03-22 12:04:17 +0300

    Update ex08.lua

--- lua_lesson_4/ex08.lua
+++ lua_lesson_4/ex08.lua
@@ -1,26 +1,12 @@
 -- Программа запрашивает число N. Вывести на экран N сумм вида 1 + 2 + ... + i, где i меняется от 1 до N.
 
+print("Введите число N:")
+local N = tonumber(io.read())
 
-
-
--- Простой вывод на экран
-print("Введите первое число")
-local n = assert(tonumber(io.read('*l')), 'Need number!')
-
-for i = 1, n do
-   io.write(i .. "+")
+for i = 1, N do
+  local sum = 0
+  for j = 1, i do
+    sum = sum + j
+  end
+  print("сумма цифр от 1 до " .. i .. ": " .. sum)
 end
-io.write("\b") -- делает возврат на один символ
-
-
--- Вывод на экран с решением
-print("Введите первое число")
-local n = assert(tonumber(io.read('*l')), 'Need number!')
-
-local sum = 0
-
-for i = 1, n do
-    io.write(i .. " + ")
-   sum = sum + i
-end
-io.write("\b = " .. sum)
