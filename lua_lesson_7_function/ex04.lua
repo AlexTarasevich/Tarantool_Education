ecdf691426c4bcbb2d95da7b7b948d7c3ef9cbc5
Александр Тарасевич <66253091+AlexTarasevich@users.noreply.github.com>
2024-05-03 22:23:44 +0300

    Update ex04.lua

--- lua_lesson_7_function/ex04.lua
+++ lua_lesson_7_function/ex04.lua
@@ -1,7 +1,7 @@
 -- Написать функцию, которая в качестве аргумента принимает произвольное целое положительное число и считает сумму его цифр. 
 -- Подсчёт суммы цифр реализовать итеративным способом.
 
---local n =  assert(tonumber(io.read('*l')), 'Need number!')
+local n =  assert(tonumber(io.read('*l')), 'Need number!')
 
 function sum_of (n)
     local sum = 0
